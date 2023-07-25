50c11cd0b6b023b18b16a2b2287c3ebb9dace66d
MahRukh <mahrukhdev@gmail.com>
2023-07-25 13:08:47 +0500

    birthday Wish

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,61 @@
 fun main() {
-    print("Happy Birthday!\n")
-    print("Jhansi\n")
-    println("You are 25!")
 
-    println("no line break")
+    val age = 24
+    val name = "Rover"
+    val layers = 5
+
+    val border = "`._,-'"
+    val timesToRepeat = 4
+    printBorder(border, timesToRepeat)
+    println("Happy Birthday, ${name}")
+    printBorder(border, timesToRepeat)
+
+    // Let's print a cake!
+    printCakeCandles(age)
+    printCakeTop(age)
+    printCakeBottom(age,layers)
+
+    //this prints an empty line
     println("")
-    println("with line \n break")
 
+    println("You are already ${age} days old, ${name}!")
+    println("${age} days old is the very best age to celebrate!")
+
+}
+
+fun printBorder(border: String, timesToRepeat: Int){
+    repeat(timesToRepeat){
+        print(border)
+    }
+    println()
+}
+
+fun printCakeCandles(age: Int){
+    print(" ")
+    repeat(age) {
+        print(",")
+    }
+    println() // Print an empty line
+
+    print(" ") // Print the inset of the candles on the cake
+    repeat(age) {
+        print("|")
+    }
+    println()
+}
+
+fun printCakeTop(age:Int){
+    repeat(age + 2) {
+        print("=")
+    }
+    println()
+}
+
+fun printCakeBottom(age:Int, layers:Int){
+    repeat(layers) {
+        repeat(age + 2) {
+            print("@")
+        }
+        println()
+    }
 }
